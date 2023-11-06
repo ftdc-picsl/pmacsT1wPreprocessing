7a410c835db380ba6933c08a20e4572cdf8760c9
Philip A Cook <cookpa@pennmedicine.upenn.edu>
2023-11-06 15:40:04 -0500

    ENH: Bump container version (allow session input)

--- bin/submit_preproc.sh
+++ bin/submit_preproc.sh
@@ -60,7 +60,7 @@ bsub -cwd . -o "${outputBIDS}/code/logs/ftdc-t1w-preproc_${date}_%J.txt"\
     -gpu "num=1:mode=exclusive_process:mps=no" \
     singularity run --containall --nv \
     -B /scratch:/tmp,${inputBIDS}:/input,${outputBIDS}:/output,${participantList}:/participants/list.txt \
-    ${repoDir}/containers/ftdc-t1w-preproc-0.2.0.sif \
+    ${repoDir}/containers/ftdc-t1w-preproc-0.2.1.sif \
     --input-dataset /input \
     --output-dataset /output \
     --participant /participants/list.txt
